c0e03bafa1ffbac57380ce119b0f172d4ee6b2b0
MrDanielHarka <daniel@harka.com>
2021-12-13 12:43:38 +0100

    Add new practice

---
+++ angular-material-design-menu/README.md
@@ -0,0 +1,3 @@
+# Reszponzív Angular Material Design Menu & Random Gombok
+
+Kipróbálható: [okj.harka.com/angular-material-design-menu](https://okj.harka.com/angular-material-design-menu/)
---
+++ angular-material-design-menu/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [AppComponent, MainNavComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
